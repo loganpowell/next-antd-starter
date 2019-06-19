fa5fb997a17e05cda96f1b26940471901367c4bc
Logan Powell <loganp@tepper.cmu.edu>
2019-06-19 10:26:41 -0400

    cleaning up...

--- pages/index.js
+++ pages/index.js
@@ -15,93 +15,90 @@ import { DatePicker, Table, Divider, Tag } from "antd"
 const { Column, ColumnGroup } = Table;
 
 
-const fetcher = async (url, key) => {
-  const result = await fetch(url, {
-    method: "GET",
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization-Key': key,
-      'User-Agent': "loganpowell@gmail.com",
-      'Host': 'data.usajobs.gov'
-    }
-  })
+// const fetcher = async (url, key) => {
+//   const result = await fetch(url, {
+//     method: "GET",
+//     headers: {
+//       '...' : '...'
+//     }
+//   })
   
-  const prime = await result.json()
-  console.log("api call: " + url);
-  // console.log("SearchResult:")
-  const { SearchResult: { SearchResultItems } } = prime
-  // console.log(SearchResultItems)
-  return SearchResultItems.map(({ MatchedObjectDescriptor }, idx) => {
-    const {
-      ApplicationCloseDate,
-      ApplyURI: [ applyLink ],
-      DepartmentName,
-      JobCategory: [ { Name: catagory } ],
-      OrganizationName,
-      PositionEndDate: applyBy,
-      PositionLocation: [ { CityName, CountryCode, CountrySubDivisionCode, Latitude, LocationName, Longitude } ],
-      PositionOfferingType: [ { Name : appointmentType } ],
-      PositionRemuneration: [ { MinimumRange: minPay, MaximumRange: maxPay, RateIntervalCode: per} ],
-      PositionTitle,
-      PositionURI
-    } = MatchedObjectDescriptor
-    return {
-      key: idx,
-      positionTitle: PositionTitle,
-      deadline: applyBy,
-      location: LocationName,
-      tags: DepartmentName,
-      link: applyLink,
-      payRange: `$${Number(minPay).toFixed(0)} to $${Number(maxPay).toFixed(0)} ${per}`
-    }
-  }) // summaries is an object
-}
+//   const prime = await result.json()
+//   console.log("api call: " + url);
+//   // console.log("SearchResult:")
+//   const { SearchResult: { SearchResultItems } } = prime
+//   // console.log(SearchResultItems)
+//   return SearchResultItems.map(({ MatchedObjectDescriptor }, idx) => {
+//     const {
+//       ApplicationCloseDate,
+//       ApplyURI: [ applyLink ],
+//       DepartmentName,
+//       JobCategory: [ { Name: catagory } ],
+//       OrganizationName,
+//       PositionEndDate: applyBy,
+//       PositionLocation: [ { CityName, CountryCode, CountrySubDivisionCode, Latitude, LocationName, Longitude } ],
+//       PositionOfferingType: [ { Name : appointmentType } ],
+//       PositionRemuneration: [ { MinimumRange: minPay, MaximumRange: maxPay, RateIntervalCode: per} ],
+//       PositionTitle,
+//       PositionURI
+//     } = MatchedObjectDescriptor
+//     return {
+//       key: idx,
+//       positionTitle: PositionTitle,
+//       deadline: applyBy,
+//       location: LocationName,
+//       tags: DepartmentName,
+//       link: applyLink,
+//       payRange: `$${Number(minPay).toFixed(0)} to $${Number(maxPay).toFixed(0)} ${per}`
+//     }
+//   }) // summaries is an object
+// }
 
 // console.log(data)
 
-// const data = [
-//   {
-//     key: '1',
-//     positionTitle: 'John',
-//     deadline: 'Brown',
-//     age: 32,
-//     location: 'New York No. 1 Lake Park',
-//     tags: ['nice', 'developer'],
-//   },
-//   {
-//     key: '2',
-//     positionTitle: 'Jim',
-//     deadline: 'Green',
-//     age: 42,
-//     location: 'London No. 1 Lake Park',
-//     tags: ['loser'],
-//   },
-//   {
-//     key: '3',
-//     positionTitle: 'Joe',
-//     deadline: 'Black',
-//     age: 32,
-//     location: 'Sidney No. 1 Lake Park',
-//     tags: ['cool', 'teacher'],
-//   },
-// ];
+const data = [
+  {
+    key: '1',
+    positionTitle: 'John',
+    deadline: 'Brown',
+    age: 32,
+    location: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    positionTitle: 'Jim',
+    deadline: 'Green',
+    age: 42,
+    location: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    positionTitle: 'Joe',
+    deadline: 'Black',
+    age: 32,
+    location: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
 const Index = ({ router: { query: { key } } }) => {
-  const [ data, setData ] = useState([])
+  // const [ data, setData ] = useState([])
   
   // WARNING IN CONSOLE:
   // useEffect(async () => {
-  //   const res = await fetcher("https://data.usajobs.gov/api/Search?Organization=CM63&Page=1&LocationName=22202")
+  //   const res = await fetcher("url")
   //   setData(res)
   // }, [])
 
-  useEffect(() => {
-    const fetchData = async ( key ) => {
-      const res = await fetcher("https://data.usajobs.gov/api/Search?Organization=CM63&Page=1&LocationName=22202", key)
-      setData(res)
-    }
-    fetchData(key)
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async ( key ) => {
+  //     const res = await fetcher("url", key)
+  //     setData(res)
+  //   }
+  //   fetchData(key)
+  // }, [])
   return (
     <Table dataSource={data}>
       {/* <ColumnGroup title="Name"> */}
@@ -140,6 +137,6 @@ const Index = ({ router: { query: { key } } }) => {
 }
 
 Index.getInitialProps
-// const Index = fetcher("https://data.usajobs.gov/api/Search?Organization=CM63&Page=1&LocationName=22202").then(r => tabular(r))
+// const Index = fetcher("url").then(r => tabular(r))
 
 export default withPageRouter(Index)
